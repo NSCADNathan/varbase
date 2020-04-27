5d739a3195e8f132f78aa4f9befdb4d54f8c0540
NSCADNathan <nryan@nscad.ca>
2020-04-27 17:16:56 -0300

    First commit Message

---
+++ docroot/modules/contrib/varbase_layout_builder/js/vlb-configure-section.admin.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Behaviors Varbase Layout Builder general scripts.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  "use strict";
+  
+  // Configure Section Admin behaviors for Varbase Layout Builder.
+  Drupal.behaviors.varbaseLayoutBuilderConfigureSectionAdmin = {
+    attach: function (context) {
+
+      $(".vlb_background_color input:radio").each(function () {
+        $(this).next('label').addClass($(this).val());
+      });
+
+      $(".vlb_width input:radio").each(function () {
+        $(this).next('label').addClass($(this).val());
+      });
+
+      $(".vlb_gutter input:radio").each(function () {
+        $(this).next('label').addClass('gutter-' + $(this).val());
+      });
+
+      $(".vlb_column_style_3 input:radio").each(function () {
+        $(this).next('label').addClass($(this).val());
+      });
+
+      $(".vlb_column_style_2 input:radio").each(function () {
+        $(this).next('label').addClass($(this).val());
+      });
+
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
